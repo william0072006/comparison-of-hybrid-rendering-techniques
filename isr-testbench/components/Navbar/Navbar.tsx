6cad0b37bfa816b1de863881d31365a963b4d5de
Igor Stalmach <igorstalmach@outlook.com>
2025-05-19 20:07:37 +0200

    Create Navbar, Hero, Featured Cars

---
+++ isr-testbench/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+export const Navbar = () => (
+  <div className="w-[90%] h-12 bg-[rgb(29,6,9,0.8)] rounded-4xl backdrop-blur-sm sticky top-4 z-10 shadow-xs shadow-[#1D0609]">
+    <div className="flex justify-between items-center w-full h-full px-8 text-[#439361]">
+      <a href="" className="text-2xl font-bold">
+        F1 Pulse
+      </a>
+      <div className="flex space-x-8 text-xl font-medium">
+        <a href="#">Featured Cars</a>
+        <a href="#">3D Garage</a>
+        <a href="#">Race Schedule</a>
+      </div>
+    </div>
+  </div>
+);
