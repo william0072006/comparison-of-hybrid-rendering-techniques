cb6b0f5d185775b68b3a08597852e12ef6ba5cf4
Igor Stalmach <igorstalmach@outlook.com>
2025-05-19 20:07:37 +0200

    Create Navbar, Hero, Featured Cars

---
+++ ssg-testbench/components/FeaturedCars/FeaturedCarsView.tsx
@@ -0,0 +1,10 @@
+export const FeaturedCarsView = () => (
+  <div className="flex flex-col items-center w-[105%] h-220 bg-[#1D0609] mt-50 shadow-[0_-35px_30px_#1D0609]">
+    <div className="flex flex-col w-full pt-12 pl-30">
+      <h2 className="text-7xl font-bold text-[#80978F]">Featured Cars</h2>
+      <h3 className="text-2xl font-medium text-[#91BFA3] pl-0.5 mt-[-0.5rem]">
+        By user searches
+      </h3>
+    </div>
+  </div>
+);
