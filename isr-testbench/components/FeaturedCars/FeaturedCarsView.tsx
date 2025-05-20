7009e7f932f2fb28631f0dc1b6e2d0c872e96835
Igor Stalmach <igorstalmach@outlook.com>
2025-05-20 02:24:27 +0200

    Update Featured Cars and integrate with API

--- isr-testbench/components/FeaturedCars/FeaturedCarsView.tsx
+++ isr-testbench/components/FeaturedCars/FeaturedCarsView.tsx
@@ -1,10 +1,56 @@
-export const FeaturedCarsView = () => (
-  <div className="flex flex-col items-center w-[105%] h-220 bg-[#1D0609] mt-50 shadow-[0_-35px_30px_#1D0609]">
-    <div className="flex flex-col w-full pt-12 pl-30">
+"use client";
+
+import { Car } from "@/components/FeaturedCars/components";
+import { useEffect, useState } from "react";
+import { getFeaturedCars, FeaturedCarsResponse } from "@/api/getFeaturedCars";
+
+export const FeaturedCarsView = () => {
+  const [featuredCars, setFeaturedCars] = useState<FeaturedCarsResponse>([]);
+
+  useEffect(() => {
+    const fetchFeaturedCars = async () => {
+      try {
+        const cars = await getFeaturedCars();
+        setFeaturedCars(cars);
+      } catch (error) {
+        console.error("Error fetching featured cars:", error);
+      }
+    };
+
+    void fetchFeaturedCars();
+  }, []);
+
+  const renderTitle = () => (
+    <div className="flex flex-col w-full pt-14 pl-20">
       <h2 className="text-7xl font-bold text-[#80978F]">Featured Cars</h2>
       <h3 className="text-2xl font-medium text-[#91BFA3] pl-0.5 mt-[-0.5rem]">
         By user searches
       </h3>
     </div>
-  </div>
-);
+  );
+
+  const renderCars = () => (
+    <div className="flex w-full justify-around px-20 mt-12">
+      {featuredCars.map((car) => (
+        <Car
+          key={car.id}
+          imageUrl={car.imageUrl}
+          shadowColor={car.shadowColor}
+          name={car.name}
+          team={car.team}
+          engine={car.engine}
+        />
+      ))}
+    </div>
+  );
+
+  return (
+    <div
+      className="flex flex-col items-center w-full h-220 bg-[#1D0609] mt-38"
+      id="featured-cars"
+    >
+      {renderTitle()}
+      {renderCars()}
+    </div>
+  );
+};
--- isr-testbench/next.config.ts
+++ isr-testbench/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL("http://localhost:8000/**")],
+  },
 };
 
 export default nextConfig;
