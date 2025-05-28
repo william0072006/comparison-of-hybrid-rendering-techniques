a9afec2638ac4ca50f2f029836c89dcff668dcc5
Igor Stalmach <igorstalmach@outlook.com>
2025-05-28 21:34:27 +0200

    Update project name

--- rsc-testbench/app/layout.tsx
+++ rsc-testbench/app/layout.tsx
@@ -14,8 +14,8 @@ const shrikhand = Shrikhand({
 });
 
 export const metadata: Metadata = {
-  title: "F1 Pulse - RCS",
-  description: "A test website for RCS rendering technique.",
+  title: "F1 Pulse - RSC",
+  description: "A test website for RSC rendering technique.",
 };
 
 export const dynamic = "force-dynamic";
