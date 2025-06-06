573951eb9fd8d4d9b875038cbc73cf1a7d7570ca
Igor Stalmach <igorstalmach@outlook.com>
2025-06-06 16:53:23 +0200

    Add artificial complexity & bloat

--- ssg-testbench/next.config.ts
+++ ssg-testbench/next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("http://localhost:8000/**")],
-    unoptimized: true,
   },
-  output: "export",
 };
 
 export default nextConfig;
